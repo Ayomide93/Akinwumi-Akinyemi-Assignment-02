3ec106b76762fa998da175f17b78e54192827d35
Akinwumi Akinyemi Akinyemi <102488211+Ayomide93@users.noreply.github.com>
2022-07-13 14:47:23 +0000

    did challenge 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let total = 0;
+
+  for(let i in arrayOfNumbers) {
+    total += arrayOfNumbers[i];
+  }
+  return total;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let even = 0;
+  for(let i in arrayOfNumbers){
+    even += (arrayOfNumbers[i] % 2 === 0);
+  }  
+  return even
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
